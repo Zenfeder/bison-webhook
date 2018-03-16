4dad7027f10f9b7bfefe0c1ce308f47ccc582fd9
CG <1054609489@qq.com>
2018-03-16 21:54:54 +0800

    Docker自动化

--- src/route.js
+++ src/route.js
@@ -9,19 +9,19 @@ module.exports = function(app) {
         .post((req, res, next) => {
             shell.cd('../bison-api')
             shell.exec('git pull')
-            shell.exec('npm install')
-            shell.exec('pm2 restart bison-api')
+            shell.exec('docker image build -t bison-api .')
+            shell.exec('docker container run --rm -p 8081:8081 -it bison-api')
             res.json({
                 message: `${req.body}`
             })
         })
 
-    app.route('/bison-spa')
+    app.route('/bison-h5')
         .post((req, res, next) => {
-            shell.cd('../bison-spa')
+            shell.cd('../bison-h5')
             shell.exec('git pull')
-            shell.exec('npm install')
-            shell.exec('pm2 restart bison-spa')
+            shell.exec('docker image build -t bison-h5 .')
+            shell.exec('docker container run --rm -p 8080:8080 -it bison-h5')
             res.json({
                 message: `${req.body}`
             })
