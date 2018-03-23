e8e01d2226bab8c311f53a5b877067e119ab17dd
CG <1054609489@qq.com>
2018-03-23 16:52:25 +0800

    hook update

--- src/route.js
+++ src/route.js
@@ -7,27 +7,31 @@ module.exports = function(app) {
 
     app.route('/bison-api')
         .post((req, res, next) => {
+            let targetDir = '../bison-api'
+
+            execSync('git pull', { cwd: targetDir })
+
             res.json({
                 message: `${req.body}`
             })
 
-            let targetDir = '../bison-api'
-
-            execSync('git pull', { cwd: targetDir })
-            execSync('docker image build -t bison-api .', { cwd: targetDir })
-            execSync('docker container run --rm -p 8081:8081 bison-api', { cwd: targetDir })
+            execSync('docker image build -t bison-api .', { cwd: targetDir })            
+            execSync('docker container run --rm -it -p 8081:8081 bison-api', { cwd: targetDir })
+            execSync('docker image prune')
         })
 
     app.route('/bison-h5')
         .post((req, res, next) => {
+            let targetDir = '../bison-h5'
+            
+            execSync('git pull', { cwd: targetDir })
+
             res.json({
                 message: `${req.body}`
             })
 
-            let targetDir = '../bison-h5'
-            
-            execSync('git pull', { cwd: targetDir })
             execSync('docker image build -t bison-h5 .', { cwd: targetDir })
             execSync('docker container run --rm -p 8080:8080 bison-h5', { cwd: targetDir })
+            execSync('docker image prune')
         })
 }
