9d968c73c396732d45c816629042ac92b1c15da8
CG <1054609489@qq.com>
2018-03-28 17:22:35 +0800

    modify project name

--- src/route.js
+++ src/route.js
@@ -5,29 +5,29 @@ module.exports = function(app) {
         next()
     })
 
-    app.route('/bison-api')
+    app.route('/ha-kafer-api')
         .post((req, res, next) => {
-            let targetDir = '../bison-api'
+            let targetDir = '../ha-kafer-api'
 
             execSync('git pull', { cwd: targetDir })
 
             execSync('npm i', { cwd: targetDir })
-            execSync('pm2 restart bison-api', { cwd: targetDir })
+            execSync('pm2 restart ha-kafer-api', { cwd: targetDir })
             // execSync('npm run start', { cwd: targetDir })
 
             res.json({
                 message: '发布成功'
             })
 
-            // execSync('docker image build -t bison-api .', { cwd: targetDir })            
-            // execSync('docker container kill bison-api', { cwd: targetDir })
-            // execSync('docker container run --name bison-api --rm -p 8081:8081 bison-api', { cwd: targetDir })
+            // execSync('docker image build -t ha-kafer-api .', { cwd: targetDir })            
+            // execSync('docker container kill ha-kafer-api', { cwd: targetDir })
+            // execSync('docker container run --name ha-kafer-api --rm -p 8081:8081 ha-kafer-api', { cwd: targetDir })
             // execSync('docker image prune')
         })
 
-    app.route('/bison-h5')
+    app.route('/ha-kafer-h5')
         .post((req, res, next) => {
-            let targetDir = '../bison-h5'
+            let targetDir = '../ha-kafer-h5'
             
             execSync('git pull', { cwd: targetDir })
 
@@ -37,13 +37,13 @@ module.exports = function(app) {
             
             execSync('npm i', { cwd: targetDir })
             execSync('npm run build', { cwd: targetDir })
-            execSync('pm2 restart bison-h5', { cwd: targetDir })
+            execSync('pm2 restart ha-kafer-h5', { cwd: targetDir })
             // execSync('npm run deploy', { cwd: targetDir })
 
 
-            // execSync('docker image build -t bison-h5 .', { cwd: targetDir })
-            // execSync('docker container kill bison-h5', { cwd: targetDir })
-            // execSync('docker container run --name bison-h5 --rm -p 8080:8080 bison-h5', { cwd: targetDir })
+            // execSync('docker image build -t ha-kafer-h5 .', { cwd: targetDir })
+            // execSync('docker container kill ha-kafer-h5', { cwd: targetDir })
+            // execSync('docker container run --name ha-kafer-h5 --rm -p 8080:8080 ha-kafer-h5', { cwd: targetDir })
             // execSync('docker image prune')
         })
 }
